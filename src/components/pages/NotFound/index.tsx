3e167d806c313261448488c4be4dbf106569565a
Yuji Matsumoto (queq1890) <yuji.matsumoto@sun-asterisk.com>
2020-11-13 14:25:18 +0700

    Setup tailwind css

--- src/components/pages/NotFound/index.tsx
+++ src/components/pages/NotFound/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
-const NotFound = () => <div>Not Found</div>;
+const NotFound = () => (
+  <div className="sm:flex sm:items-center px-6 py-4">Not Found</div>
+);
 
 export default NotFound;
